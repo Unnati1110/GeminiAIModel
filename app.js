1959e70f4e6e87a811cf2b0db3c46db9b76e2df4
Unnati1110 <unnatisurpatne1110@gmail.com>
2025-06-09 11:30:30 +0530

    ...

--- app.js
+++ app.js
@@ -6,22 +6,18 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 app.use(bodyParser.json()); 
 
 app.post("/getResponse", (req, res) => {
-  const question = req.body.question; 
-
-  if (!question) {
-    return res.status(400).json({ error: "Question is required" });
-  }
-
+  console.log(req.body.question);
   const genAI = new GoogleGenerativeAI('AIzaSyA7n5AQkuX0-47k7dhpr4UYhBbC-JQcH4Y');
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  model.generateContent(question)
+  model.generateContent(req.body.question)
     .then((result) => {
+      console.log(result.response.text());
       const responseText = result.response.text();
       res.status(200).json({ response: responseText });
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).json({ error: "Failed to generate response" });
+      res.status(500).json({ msg: "bad request" });
     });
 });
